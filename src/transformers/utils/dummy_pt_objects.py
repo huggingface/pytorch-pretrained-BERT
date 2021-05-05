b698a6edf19e29ac33dae3915ba9f5ddaadaa760
Niels Rogge <niels.rogge1@gmail.com>
2021-05-05 10:43:46 +0200

    Some more improvements

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -1105,6 +1105,11 @@ class DetrForObjectDetection:
         requires_backends(self, ["torch"])
 
 
+class DetrForSegmentation:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class DetrModel:
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["torch"])
